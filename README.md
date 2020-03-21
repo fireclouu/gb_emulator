0480119466196940b6902e8ec9c457b5520fd6c9
fireclouu <imfireclouu@gmail.com>
2020-03-21 16:51:41 +0800

    removed old files

--- README.md
+++
@@ -1,4 +0,0 @@
-# GB emulator
-My attempt to create Gameboy emulator in C
-
-- Using SDL2 for handling display
--- src/includes/cycle.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CYCLE_H
-#define _CYCLE_H
-
-#include <stdio.h>
-
-extern const uint8_t CPU_CYCLE[256 * 2];
-extern const uint8_t BYTE_USED[256 * 2];
-
-#endif // header guard
